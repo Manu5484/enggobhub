d478ac0407cd688b97d936505a5a4eb7030e23ad
Nagaraj2176 <nagrajgithub76@gmail.com>
2025-03-03 20:50:53 +0530

    headr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,19 @@ export default function Header()
   const logo = "/engjobhu_icon.jpg";
   return(
     
-    <nav>
-      <img src={logo} alt="logo"></img>
+    <nav className="headernav">
+      <img src={logo} alt="logo" className="logoimage"></img>
+
+      <div class="InputContainer">
+      <input placeholder="Search.." id="input" class="input" name="text" type="text"/>
+      </div>
+
+      <button class="btn">Home</button>
+      <button class="btn">About</button>
+      <button class="btn">Contact</button>
+      <button class="btn">Blog</button>
+      <button class="btn">Register</button>
+      
     </nav>
   )
 }

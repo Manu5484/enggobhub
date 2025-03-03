e0f07ccf56600f34aa17ca426c50af78d2de9efe
Nagaraj2176 <nagrajgithub76@gmail.com>
2025-03-03 20:53:31 +0530

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ export default function Header()
   // const logo = "/engjobhu_icon.jpg";
   return(
     
-<<<<<<< HEAD
     <nav className="headernav">
       <img src={logo} alt="logo" className="logoimage"></img>
 
@@ -20,11 +19,6 @@ export default function Header()
       <button class="btn">Contact</button>
       <button class="btn">Blog</button>
       <button class="btn">Register</button>
-      
-=======
-    <nav>
-      <h1>header</h1>
->>>>>>> a9d442261a136fee649a9e2039222a199f8b4a56
-    </nav>
+      </nav>
   )
 }

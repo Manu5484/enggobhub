ae47bad117b7368f4a7bc197aa2577b410359c12
Manu5484 <manojks2402@gmail.com>
2025-03-04 00:08:09 +0530

    added link to nav elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../static/Header.css"
 import searchbutton from "../static/images/search_button.png"
+import {Link} from 'react-router-dom'
 
 export default function Header()
 {
@@ -16,11 +17,21 @@ export default function Header()
       </div>
       
       <div className="btncontainer">
-        <button class="btn">Home</button>
-        <button class="btn">About</button>
-        <button class="btn">Contact</button>
-        <button class="btn">Blog</button>
-        <button class="btn">Register</button>
+        <Link to={'/'}>
+          <button class="btn">Home</button>
+        </Link>
+        <Link to="#">
+          <button class="btn">About</button>
+        </Link>
+        <Link to={"#"}>
+          <button class="btn">Contact</button>
+        </Link>
+        <Link to={"/blog"}>
+          <button class="btn">Blog</button>
+        </Link>
+        <Link to={"#"}>
+          <button class="btn">Register</button>
+        </Link>
       </div>
       
     </nav>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,9 @@ import React from "react";
 export default function Blog()
 {
   return (
-    <h1>blog page</h1>
+    <>
+     <h1>blog page</h1>
+     <h1>blog page</h1>
+    </>
   )
 }

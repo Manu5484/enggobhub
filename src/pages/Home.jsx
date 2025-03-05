14ef14e392681fc64c40ae713fb4e9bfb92cee3b
Manu5484 <manojks2402@gmail.com>
2025-03-05 12:33:40 +0530

    home page form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import SliderHome from "../components/SliderHome"
+import SliderHome from "../components/SliderHome";
+import HomePageForm from "../components/HomePageForm";
 import CardSlider from "../components/CardSlider";
 
 export default function Home()
@@ -26,6 +27,7 @@ export default function Home()
         <SliderHome slides={slides} />
       </div>
       <CardSlider/>
+      <HomePageForm/>
     </>
   )
 }

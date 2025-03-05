4fbc70f5b34d45b30e120733a8621b7f7de6a8e4
Nagaraj2176 <nagrajgithub76@gmail.com>
2025-03-05 12:59:30 +0530

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import SliderHome from "../components/SliderHome";
 import HomePageForm from "../components/HomePageForm";
 import CardSlider from "../components/CardSlider";
-
+import CompanySlider from "../components/CompanySlider";
 export default function Home()
 {
   const slides = [
@@ -28,6 +28,7 @@ export default function Home()
       </div>
       <CardSlider/>
       <HomePageForm/>
+      <CompanySlider/>
     </>
   )
 }

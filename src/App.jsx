6497cf531558d7dafaeb8a322cec7dd592ba7ab6
Nagaraj2176 <nagrajgithub76@gmail.com>
2025-03-10 09:50:27 +0530

    Login done

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,47 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import Login1 from './pages/Login1';
+import {HashRouter as Router, Routes, Route,useLocation} from 'react-router-dom'
 import './App.css';
 
-function App() {
+
+function Layout() {
+  const location = useLocation();
+  const hideFooter = location.pathname === "/login";
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-      </Routes>
-      <Footer/>
-    </Router>
+      <div>
+        <Header/>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/blog" element={<Blog/>}/>
+              <Route path="/login" element={<Login1 />} />
+          </Routes>
+
+          {!hideFooter && <Footer />}
+      </div>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <Router>
+//       <Header/>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/blog" element={<Blog/>}/>
+//         <Route path="/login" element={<Login1/>}/>
+//       </Routes>
+//       <Footer/>
+//     </Router>
+//   );
+// }
+
+export default function App() {
+  return (
+      <Router>
+          <Layout />
+      </Router>
+  );
+}
+

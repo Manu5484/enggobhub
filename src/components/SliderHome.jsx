e561e26b3e76a3066fc537f060ffe171db80e60e
Manu5484 <manojks2402@gmail.com>
2025-03-21 12:29:52 +0530

    auto sliding home slider

--- src/components/SliderHome.jsx
+++ src/components/SliderHome.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../static/SliderHome.css"
-import slider1 from "../static/images/slider1.jpg"
-import slider2 from "../static/images/corporate_blog.jpg"
-import slider3 from "../static/images/cybersecurity_blog.jpg"
+import "../static/SliderHome.css";
+import slider1 from "../static/images/slider1.jpg";
+import slider2 from "../static/images/corporate_blog.jpg";
+import slider3 from "../static/images/cybersecurity_blog.jpg";
 
 function SliderHome() {
+  const sliderRef = useRef(null); 
+
   const settings = {
     dots: true,
     infinite: true,
@@ -15,18 +17,29 @@ function SliderHome() {
     slidesToShow: 1,
     slidesToScroll: 1
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.slickNext(); 
+      }
+    }, 3000); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div className="home-slider-container">
       <div className="slider-container">
-        <Slider {...settings}>
-          <div  className="sliderimgdiv">
-            <img src={slider1} alt="slider"></img>
+        <Slider ref={sliderRef} {...settings}>
+          <div className="sliderimgdiv">
+            <img src={slider1} alt="slider" />
           </div>
           <div className="sliderimgdiv">
-            <img src={slider2} alt="slider"></img>
+            <img src={slider2} alt="slider" />
           </div>
           <div className="sliderimgdiv">
-            <img src={slider3} alt="slider"></img>
+            <img src={slider3} alt="slider" />
           </div>
         </Slider>
       </div>

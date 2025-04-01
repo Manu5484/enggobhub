bd1c01e979cc213b84699e97e5cf8f0b91fe02f3
Manu5484 <manojks2402@gmail.com>
2025-04-01 09:48:48 +0530

    resize

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,73 +10,77 @@ export default function Header() {
   const logo = "/engjobhu_icon.jpg";
 
   return (
-    <nav className="headernav">
-      <img src={logo} alt="logo" className="logoimage" />
-      <span className="logoname">ENGJOBHUB</span>
-      <div className="InputContainer">
-        <input placeholder="Search.." id="input" className="input" name="text" type="text" />
-        <button className="search-btn">
-          <img src={searchbutton} alt="search" />
-        </button>
-      </div>
-      <div className="btncontainer">
-        <NavLink to={'/'}>
-          <button className="btnnav">Home</button>
-        </NavLink>
-
-        {/* Opportunities Dropdown */}
-        <div className="dropdown-container"
-          onMouseEnter={() => setShowOpportunities(true)}
-          onMouseLeave={() => setShowOpportunities(false)}>
-          <button className="btnnav">Opportunities</button>
-          {showOpportunities && (
-            <div className="dropdown-menu">
-              <div className="dropdown-left">
-                <NavLink to="/internship">Internships</NavLink>
-                <hr />
-                <NavLink to="/internships-bangalore">Interns In Bangalore</NavLink>
-                <NavLink to="/internships-remote">Work From Home</NavLink>
-              </div>
-              <div className="dropdown-right">
-                <NavLink to="/jobs">Jobs</NavLink>
-                <hr />
-                <NavLink to="/jobs-bangalore">Jobs In Bangalore</NavLink>
-                <NavLink to="/jobs-remote">Work From Home</NavLink>
-              </div>
-            </div>
-          )}
+    <div className="header-container">
+      <nav className="headernav">
+        <div className="left-header">
+        <img src={logo} alt="logo" className="logoimage" />
+        <span className="logoname">ENGJOBHUB</span>
         </div>
-
-        {/* Services Dropdown */}
-        <div className="dropdown-container"
-          onMouseEnter={() => setShowServices(true)}
-          onMouseLeave={() => setShowServices(false)}>
-          <button className="btnnav">Services</button>
-          {showServices && (
-            <div className="dropdown-menu">
-              <div className="dropdown-left">
-                <NavLink to="/internship">Courses</NavLink>
-                <hr />
-                <NavLink to="/internships-bangalore">Free Courses</NavLink>
-                <NavLink to="/internships-remote">Paid Courses</NavLink>
-              </div>
-              <div className="dropdown-right">
-                <NavLink to="/jobs">Resume</NavLink>
-                <hr />
-                <NavLink to="/jobs-bangalore">AI Resume Builder</NavLink>
-                <NavLink to="/resumetemplates">Resume Template</NavLink>
-              </div>
-            </div>
-          )}
+        <div className="InputContainer">
+          <input placeholder="Search.." id="input" className="input" name="text" type="text" />
+          <button className="search-btn">
+            <img src={searchbutton} alt="search" />
+          </button>
         </div>
+        <div className="btncontainer">
+          <NavLink to={'/'}>
+            <button className="btnnav">Home</button>
+          </NavLink>
 
-        <NavLink to={"/blog"}>
-          <button className="btnnav">Blog</button>
-        </NavLink>
-        <NavLink to={"/login"}>
-          <button className="btnnav">Register</button>
-        </NavLink>
+          {/* Opportunities Dropdown */}
+          <div className="dropdown-container"
+            onMouseEnter={() => setShowOpportunities(true)}
+            onMouseLeave={() => setShowOpportunities(false)}>
+            <button className="btnnav">Opportunities</button>
+            {showOpportunities && (
+              <div className="dropdown-menu">
+                <div className="dropdown-left">
+                  <NavLink to="/internship">Internships</NavLink>
+                  <hr />
+                  <NavLink to="/internships-bangalore">Interns In Bangalore</NavLink>
+                  <NavLink to="/internships-remote">Work From Home</NavLink>
+                </div>
+                <div className="dropdown-right">
+                  <NavLink to="/jobs">Jobs</NavLink>
+                  <hr />
+                  <NavLink to="/jobs-bangalore">Jobs In Bangalore</NavLink>
+                  <NavLink to="/jobs-remote">Work From Home</NavLink>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* Services Dropdown */}
+          <div className="dropdown-container"
+            onMouseEnter={() => setShowServices(true)}
+            onMouseLeave={() => setShowServices(false)}>
+            <button className="btnnav">Services</button>
+            {showServices && (
+              <div className="dropdown-menu">
+                <div className="dropdown-left">
+                  <NavLink to="/internship">Courses</NavLink>
+                  <hr />
+                  <NavLink to="/internships-bangalore">Free Courses</NavLink>
+                  <NavLink to="/internships-remote">Paid Courses</NavLink>
+                </div>
+                <div className="dropdown-right">
+                  <NavLink to="/jobs">Resume</NavLink>
+                  <hr />
+                  <NavLink to="/jobs-bangalore">AI Resume Builder</NavLink>
+                  <NavLink to="/resumetemplates">Resume Template</NavLink>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <NavLink to={"/blog"}>
+            <button className="btnnav">Blog</button>
+          </NavLink>
+          <NavLink to={"/login"}>
+            <button className="btnnav">Register</button>
+          </NavLink>
+        </div>
+        </nav>
       </div>
-    </nav>
   );
 }
